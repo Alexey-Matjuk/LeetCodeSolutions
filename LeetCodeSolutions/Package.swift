b06c0ed1375706e9c563419ceb67767c909721a0
Alexey Matjuk <bokkusunotegami@gmail.com>
2025-01-12 20:51:55 +0800

    Add project package

---
+++ LeetCodeSolutions/Package.swift
@@ -0,0 +1,27 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import Foundation
+import PackageDescription
+
+/// Auto-generated list of solutions
+let solutions = [
+]
+
+let package = Package(
+    name: "LeetCodeSolutions",
+    platforms: [.macOS(.v15)],
+    targets: solutions.reduce([]) {
+        $0 + [
+            .target(
+                name: $1,
+                path: "\($1)/Sources"
+            ),
+            .testTarget(
+                name: "\($1)Tests",
+                dependencies: [.byName(name: $1)],
+                path: "\($1)/Tests"
+            )
+        ]
+    }
+)
